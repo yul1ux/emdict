fce30c5af4108362d85f132a7886614f98ed22a8
Soe Thiha <soethiha5125@gmail.com>
2022-10-06 19:51:22 +0630

    just format document

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,21 +1,21 @@
-const { getDefService } = require('../../services/index.js');
-const { Api400Error, Api404Error } = require('../../utils/ApiError');
+const { getDefService } = require("../../services/index.js")
+const { Api400Error, Api404Error } = require("../../utils/ApiError")
 
 const getDef = async (req, res, next) => {
-  const word = req.query.w;
+  const word = req.query.w
 
   try {
     if (!word) {
-      throw new Api400Error('Please fill a word');
+      throw new Api400Error("Please fill a word")
     }
-    const def = await getDefService(word);
+    const def = await getDefService(word)
     if (def[0] === undefined) {
-      throw new Api404Error('No definition found!');
+      throw new Api404Error("No definition found!")
     }
-    res.status(200).json(def);
+    res.status(200).json(def)
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
-module.exports = { getDef };
+module.exports = { getDef }
