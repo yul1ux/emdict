56d5c162d6deec19dbb7306ed584f640876772a4
Soe Thiha <soethiha5125@gmail.com>
2022-10-06 19:57:44 +0630

    add try-catch statement

--- src/utils/mongodb.js
+++ src/utils/mongodb.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose')
-const config = require('../config');
+const mongoose = require("mongoose")
+const config = require("../config")
 
-const dbConnection =  async () => {
-    const connection = await mongoose.connect(config.DB,{useNewUrlParser :true,useUnifiedTopology:true})
-    
-    return connection.db;
-   
+const dbConnection = async () => {
+  try {
+    const connection = await mongoose.connect(config.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
+    return connection.db
+  } catch (err) {
+    throw new Error(err)
+  }
 }
 
-
-module.exports = dbConnection;
+module.exports = dbConnection
