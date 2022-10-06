e70a9e13018c6b65326bd959f0db39a409aa1fd8
Soe Thiha <soethiha5125@gmail.com>
2022-10-06 20:08:17 +0630

    change development var

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const dotenv = require('dotenv');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -7,12 +8,17 @@ const env = dotenv.config();
 if (env.error) {
   throw new Error('dotenv file not found');
 }
-
+let dbUrl= process.env.MONGODB_URL
+let redisUrl = process.env.REDIS_URL
+if(process.env.NODE_ENV == 'development'){
+  dbUrl = process.env.MONGODB_DURL
+  redisUrl = process.env.REDIS_DURL
+}
 module.exports = {
   PORT: process.env.PORT,
 
-  DB:process.env.MONGODB_DURI,
-  REDIS : process.env.REDIS_DURI,
+  DB:process.env.dbUrl,
+  REDIS : process.env.redisUrl,
   api: {
     prefix: '/api',
   },
