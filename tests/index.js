56024d6751a9b6f1f4cff2e742d35cb746da03f1
Soe Thiha <soethiha5125@gmail.com>
2022-10-06 20:00:11 +0630

    add integration testing

--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,21 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 const request = require('supertest')
 const expect = require('chai').expect
-
-const url =  request('https://e-mdict.herokuapp.com')
+const app = require('../src/app.js')
 describe('Health check',function(){
-    it('return ok',async()=>{ 
-        const res =await url.get('/status')
-        expect(res.status).to.eql(200)
+    it('return ok',function (done){ 
+        request(app)
+          .get('/status')
+          .expect(200,done)
+          
     })
 })
 describe('get definition',function(){
-    it('return def and state',async ()=>{
-        const res =await url.get('/api/search?w=dog')
-        expect(res.status).to.eql(200)
+    it('return def and state', (done)=>{
+        request(app)
+          .get('/api/search?w=dog')
+          .expect(200,done)
+          
     })
 })
